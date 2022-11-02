3b5bbfe25245107431727e3fd1f92f7b010f3652
noob <115921421+srijanB7@users.noreply.github.com>
2022-11-02 09:55:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+//Quiz CLI to test knowledege of football
+let readline = require('readline-sync');
+
+let globalScore = 0;
+
+let questions = [
+    {
+        question : "Batman is from which Universe DC or Marvel ? ",
+        answer : "DC"
+    },
+    
+    {
+        question : "Clark Kent is real name of which super hero ",
+        answer : "Superman"
+    },
+
+    {
+        question: "Is wolverine from DC",
+        answer: "no"
+    },
+
+    {
+        question: "Which number robin is jason todd",
+        answer: "2"
+    },
+
+]
+
+// function to check answers of questions
+function checkAnswer(input, answer) {
+    if(input === answer)
+        return true;
+    return false;
+}
+
+const name = readline.question("What is your Name ? ");
+console.log("Hello " + name );
+console.log("lets start with the quiz");
+
+let answer;
+for(let i = 0; i < questions.length; i++) {
+    console.log(`Question ${i+1}` );
+    answer = readline.question(questions[i].question);
+    console.log(questions[i].answer);
+    if(checkAnswer(answer, questions[i].answer) === true) {
+        globalScore++;
+        console.log("correct answer");
+    }
+
+}
+//final score
+console.log("Your final score is" ,globalScore);
+
+
+
+
+
